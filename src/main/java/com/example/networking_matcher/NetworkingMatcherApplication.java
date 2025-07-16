9225e651a6ecf2479acbbfdeb86bf6813c732fd5
Amelia Sommer <ameliasommer@gmail.com>
2025-07-16 21:41:57 +0100

    Initial commit

---
+++ src/main/java/com/example/networking_matcher/NetworkingMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.networking_matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingMatcherApplication.class, args);
+	}
+
+}
