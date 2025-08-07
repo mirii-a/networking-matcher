1da0b9459f0f9b9ac4300e9f0070739fbe6ba3b2
Amelia Sommer <ameliasommer@gmail.com>
2025-08-07 09:02:11 +0100

    Adding new test files for ExcelServiceTest.java and MatchingOneToOneServiceTest

---
+++ src/test/java/com/example/networking_matcher/excel/ExcelServiceTest.java
@@ -0,0 +1,24 @@
+//package com.example.networking_matcher.excel;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class ExcelServiceTest {
+//
+//    private ExcelPOIHelper excelPOIHelper;
+//    private static String FILE_NAME = "test-one-to-one-matches.xlsx";
+//    private String fileLocation;
+//
+//    @Test
+//    void getParticipantsFromSpreadsheet() {
+//    }
+//
+//    @Test
+//    void getLeadersFromSpreadsheet() {
+//    }
+//
+//    @Test
+//    void createExcelNotebook() {
+//    }
+//}
